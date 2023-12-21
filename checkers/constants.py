daecfb1d6299271414e3f21b4692af8eabfac515
JariToni <114744833+JariToni@users.noreply.github.com>
2023-12-21 17:29:38 +0100

    Initial commit

---
+++ checkers/constants.py
@@ -0,0 +1,12 @@
+import pygame
+
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH // COLS
+
+# RGB
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+GREY = (128, 128, 128)
