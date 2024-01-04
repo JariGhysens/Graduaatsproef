f514a88add5784219594b97ddbb29e68dd695ab9
JariToni <114744833+JariToni@users.noreply.github.com>
2024-01-04 12:56:17 +0100

    finished

--- checkers/board.py
+++ checkers/board.py
@@ -1,39 +1,34 @@
-import pygame 
-from .piece import Piece
+import pygame
 from .constants import BLACK, ROWS, RED, SQUARE_SIZE, COLS, WHITE
+from .piece import Piece
 
-class Board: 
+class Board:
     def __init__(self):
-        #2d lijst van de pieces
         self.board = []
         self.red_left = self.white_left = 12
         self.red_kings = self.white_kings = 0
         self.create_board()
-
-
-#inkleuren achtergrond 
+    
     def draw_squares(self, win):
         win.fill(BLACK)
         for row in range(ROWS):
             for col in range(row % 2, COLS, 2):
-                pygame.draw.rect(win, WHITE, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+                pygame.draw.rect(win, WHITE, (row*SQUARE_SIZE, col *SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
-#veplaatsen pieces 
     def move(self, piece, row, col):
         self.board[piece.row][piece.col], self.board[row][col] = self.board[row][col], self.board[piece.row][piece.col]
         piece.move(row, col)
-        if row == ROWS or row == 0:
-            piece.make_King()
+
+        if row == ROWS - 1 or row == 0:
+            piece.make_king()
             if piece.color == WHITE:
                 self.white_kings += 1
             else:
-                self.red_kings += 1
-            
-   #geselecteerde pieve         
+                self.red_kings += 1 
+
     def get_piece(self, row, col):
         return self.board[row][col]
 
-#aanmaken pieces op ons board
     def create_board(self):
         for row in range(ROWS):
             self.board.append([])
@@ -56,22 +51,100 @@ class Board:
                 if piece != 0:
                     piece.draw(win)
 
-
+    def remove(self, pieces):
+        for piece in pieces:
+            self.board[piece.row][piece.col] = 0
+            if piece != 0:
+                if piece.color == RED:
+                    self.red_left -= 1
+                else:
+                    self.white_left -= 1
+    
+    def winner(self):
+        if self.red_left <= 0:
+            return WHITE
+        elif self.white_left <= 0:
+            return RED
+        
+        return None 
+    
     def get_valid_moves(self, piece):
         moves = {}
-        left = piece.col -1
+        left = piece.col - 1
         right = piece.col + 1
         row = piece.row
 
         if piece.color == RED or piece.king:
-            pass
+            moves.update(self._traverse_left(row -1, max(row-3, -1), -1, piece.color, left))
+            moves.update(self._traverse_right(row -1, max(row-3, -1), -1, piece.color, right))
         if piece.color == WHITE or piece.king:
-            pass
+            moves.update(self._traverse_left(row +1, min(row+3, ROWS), 1, piece.color, left))
+            moves.update(self._traverse_right(row +1, min(row+3, ROWS), 1, piece.color, right))
+    
+        return moves
+
+    def _traverse_left(self, start, stop, step, color, left, skipped=[]):
+        moves = {}
+        last = []
+        for r in range(start, stop, step):
+            if left < 0:
+                break
+            
+            current = self.board[r][left]
+            if current == 0:
+                if skipped and not last:
+                    break
+                elif skipped:
+                    moves[(r, left)] = last + skipped
+                else:
+                    moves[(r, left)] = last
+                
+                if last:
+                    if step == -1:
+                        row = max(r-3, 0)
+                    else:
+                        row = min(r+3, ROWS)
+                    moves.update(self._traverse_left(r+step, row, step, color, left-1,skipped=last))
+                    moves.update(self._traverse_right(r+step, row, step, color, left+1,skipped=last))
+                break
+            elif current.color == color:
+                break
+            else:
+                last = [current]
+
+            left -= 1
         
         return moves
-    
-    def _traverse_left(self, start, stop, step, color, left, skipped=[]):
-        pass
 
     def _traverse_right(self, start, stop, step, color, right, skipped=[]):
-        pass
+        moves = {}
+        last = []
+        for r in range(start, stop, step):
+            if right >= COLS:
+                break
+            
+            current = self.board[r][right]
+            if current == 0:
+                if skipped and not last:
+                    break
+                elif skipped:
+                    moves[(r,right)] = last + skipped
+                else:
+                    moves[(r, right)] = last
+                
+                if last:
+                    if step == -1:
+                        row = max(r-3, 0)
+                    else:
+                        row = min(r+3, ROWS)
+                    moves.update(self._traverse_left(r+step, row, step, color, right-1,skipped=last))
+                    moves.update(self._traverse_right(r+step, row, step, color, right+1,skipped=last))
+                break
+            elif current.color == color:
+                break
+            else:
+                last = [current]
+
+            right += 1
+        
+        return moves
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import pygame 
-from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE
-from checkers.board import Board
+import pygame
+from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, RED
 from checkers.game import Game
 
 
@@ -9,39 +8,35 @@ FPS = 60
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Checkers')
 
-#bepalen op welke piece we hebben geklikt
 def get_row_col_from_mouse(pos):
     x, y = pos
     row = y // SQUARE_SIZE
     col = x // SQUARE_SIZE
     return row, col
 
-
 def main():
     run = True
-    clock  = pygame.time.Clock()
+    clock = pygame.time.Clock()
     game = Game(WIN)
-    
 
     while run:
         clock.tick(FPS)
 
+        if game.winner() != None:
+            print(game.winner())
+            run = False
+
         for event in pygame.event.get():
-
             if event.type == pygame.QUIT:
-                run =  False 
-
-
-
-
-#geklikt op een piece 
+                run = False
+            
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
                 row, col = get_row_col_from_mouse(pos)
-                
-        game.update()
+                game.select(row, col)
 
+        game.update()
+    
     pygame.quit()
 
 main()
-    
